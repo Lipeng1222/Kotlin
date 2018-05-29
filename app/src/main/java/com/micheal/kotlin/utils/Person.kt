485cdb5599480b11be26a8457f0d7da1e75455bd
Lipeng <lipengsc423@foxmail.com>
2018-05-29 15:03:09 +0800

    the class named 'Person' has changed

--- app/src/main/java/com/micheal/kotlin/utils/Person.kt
+++ app/src/main/java/com/micheal/kotlin/utils/Person.kt
@@ -1,4 +1,18 @@
 package com.micheal.kotlin.utils
 
-class Person {
+import kotlin.concurrent.thread
+
+class Person (name:String,age:Int){
+    private var mName:String=name
+    private var mAge:Int=age
+
+    constructor(): this("",0)
+
+    public fun setName(name:String){
+        this.mName=name
+    }
+
+    public fun setAge(age:Int){
+        this.mAge=age
+    }
 }
