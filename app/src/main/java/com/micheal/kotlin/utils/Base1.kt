375ab35560e9f093b2ec8a693e4c5620115f1af7
Lipeng <lipengsc423@foxmail.com>
2018-05-29 14:37:27 +0800

    add Person class

--- app/src/main/java/com/micheal/kotlin/utils/Base1.kt
+++ app/src/main/java/com/micheal/kotlin/utils/Base1.kt
@@ -2,11 +2,22 @@ package com.micheal.kotlin.utils
 
 class Base1{
 
+
+
+    constructor(){
+
+    }
+
+
     companion object {
         fun add(a:Int,b:Int):Int{
             return a+b;
         }
     }
+
+
+
+
 
 
 }
---
+++ app/src/main/java/com/micheal/kotlin/utils/Person.kt
@@ -0,0 +1,4 @@
+package com.micheal.kotlin.utils
+
+class Person {
+}
